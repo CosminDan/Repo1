59873b69820d22adb277025bad88ac2cc936f147
Cozmin Stefan <cozmin.stefan@activemall.ro>
2015-11-05 23:27:46 +0200

    sumchangez

--- application/language/english/pubapp_lang.php
+++ application/language/english/pubapp_lang.php
@@ -1,46 +1,68 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 
-$lang['pa_email'] = 'E-Mail';
-$lang['pa_role']  = 'Role';
-$lang['pa_language']  = 'Language';
+$lang['pa_email']    = 'E-Mail';
+$lang['pa_role']     = 'Role';
+$lang['pa_language'] = 'Language';
 $lang['pa_actions']  = 'Actions';
 
 
 
 // ARTICLES
-$lang['articles_manage']     = 'Manage Articles';
-$lang['articles_no_affiliation']     = 'No affiliation';
-$lang['articles_edit_heading']       = 'Edit Articles';
-$lang['articles_new']     = 'New';
+$lang['articles_manage']            = 'Manage Articles';
+$lang['articles_edit']              = 'Edit';
+$lang['articles_true']              = 'True';
+$lang['articles_false']             = 'False';
+$lang['articles_create']            = 'Create';
+$lang['articles_list']              = 'List';
+$lang['articles_new']               = 'New Article';
+$lang['articles_edit_text']         = 'Edit this to suit your needs';
+$lang['articles_no_records']        = 'There are no articles in the system.';
+$lang['articles_create_new']        = 'Create a new Articles.';
+$lang['articles_create_success']    = 'Articles successfully created.';
+$lang['articles_create_failure']    = 'There was a problem creating the articles: ';
+$lang['articles_create_new_button'] = 'Create New Articles';
+$lang['articles_invalid_id']        = 'Invalid Articles ID.';
+$lang['articles_edit_success']      = 'Articles successfully saved.';
+$lang['articles_edit_failure']      = 'There was a problem saving the articles: ';
+$lang['articles_delete_success']    = 'record(s) successfully deleted.';
+$lang['articles_delete_failure']    = 'We could not delete the record: ';
+$lang['articles_delete_error']      = 'You have not selected any records to delete.';
+$lang['articles_actions']           = 'Actions';
+$lang['articles_cancel']            = 'Cancel';
+$lang['articles_delete_record']     = 'Delete this Articles';
+$lang['articles_delete_confirm']    = 'Are you sure you want to delete this articles?';
+$lang['articles_edit_heading']      = 'Edit Articles';
+$lang['articles_no_affiliation']    = 'No affiliation';
 // Module Details
-$lang['articles_area_title']         = 'Articles';
+$lang['articles_area_title']        = 'Articles';
 // Fields
-$lang['articles_field_title']        = 'Title';
-$lang['articles_field_page']         = 'Page';
-$lang['articles_field_affiliation']  = 'Affiliation';
-$lang['articles_field_references']   = 'References';
-$lang['articles_field_summary']      = 'Summary';
-$lang['articles_field_tags']         = 'Tags';
-$lang['articles_field_categories']   = 'Categories';
-$lang['articles_field_authors']      = 'Author(s)';
+$lang['articles_field_title']       = 'Title';
+$lang['articles_field_page']        = 'Page';
+$lang['articles_field_affiliation'] = 'Affiliation';
+$lang['articles_field_references']  = 'References';
+$lang['articles_field_summary']     = 'Summary';
+$lang['articles_field_tags']        = 'Tags';
+$lang['articles_field_categories']  = 'Categories';
+$lang['articles_field_authors']     = 'Author(s)';
+$lang['articles_field_author_email'] = 'Corresponding Author';
 // Create/Edit Buttons
-$lang['articles_action_edit']        = 'Save Articles';
-$lang['articles_action_create']      = 'Create Articles';
+$lang['articles_action_edit']       = 'Save Articles';
+$lang['articles_action_create']     = 'Create Articles';
 
 
 
 // ISSUES
-$lang['issues_manage']             = 'Manage Issues';
+$lang['issues_manage']                 = 'Manage Issues';
 // Fields
-$lang['issues_field_title']          = 'Title';
-$lang['issues_field_number']         = 'Number';
-$lang['issues_field_special']        = 'Special Edition';
-$lang['issues_field_volume']         = 'Volume';
-$lang['issues_field_year_issue']     = 'Issue Year';
-$lang['issues_field_year_published'] = 'Published Year';
-$lang['issues_field_status']         = 'Status';
-$lang['issues_field_pdf_upload']         = 'Upload PDF';
-$lang['issues_field_cover_upload']         = 'Upload Cover Image';
+$lang['issues_field_title']            = 'Title';
+$lang['issues_field_number']           = 'Number';
+$lang['issues_field_special']          = 'Special Edition';
+$lang['issues_field_volume']           = 'Volume';
+$lang['issues_field_year_issue']       = 'Issue Year';
+$lang['issues_field_year_published']   = 'Published Year';
+$lang['issues_field_status']           = 'Status';
+$lang['issues_field_pdf_upload']       = 'Upload PDF';
+$lang['issues_field_cover_upload']     = 'Upload Cover Image';
 
 $lang['issues_heading_title']          = 'Title';
 $lang['issues_heading_number']         = '#';
@@ -52,17 +74,17 @@ $lang['issues_heading_articles_count'] = 'Articles';
 $lang['issues_heading_pdf_loaded']     = 'PDF';
 $lang['issues_heading_status']         = 'Status';
 
-$lang['issues_no_cover']         = 'No Cover Image';
+$lang['issues_no_cover']                = 'No Cover Image';
 
 // Magazines
-$lang['magazines_staff_manage'] = 'Manage Magazine Staff';
-$lang['magazines_staff_add']    = 'Add User';
-$lang['magazines_staff_new']    = 'New Member';
+$lang['magazines_staff_manage']         = 'Manage Magazine Staff';
+$lang['magazines_staff_add']            = 'Add User';
+$lang['magazines_staff_new']            = 'New Member';
 
-$lang['magazines_edit_heading']    = 'Edit Magazine';
-$lang['magazines_manage']    = 'Manage Magazines';
-$lang['magazines_new']    = 'New';
-$lang['magazines_list']    = 'List';
+$lang['magazines_edit_heading']         = 'Edit Magazine';
+$lang['magazines_manage']               = 'Manage Magazines';
+$lang['magazines_new']                  = 'New';
+$lang['magazines_list']                 = 'List';
 $lang['magazines_edit_success']         = 'Magazine edited successfully';
 
 $lang['magazines_field_title']          = 'Title';
--- application/modules/articles/language/english/articles_lang.php
+++ application/modules/articles/language/english/articles_lang.php
@@ -1,30 +1,7 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 
 
-$lang['articles_manage']             = 'Manage Articles';
-$lang['articles_edit']               = 'Edit';
-$lang['articles_true']               = 'True';
-$lang['articles_false']              = 'False';
-$lang['articles_create']             = 'Create';
-$lang['articles_list']               = 'List';
-$lang['articles_new']                = 'New Article';
-$lang['articles_edit_text']          = 'Edit this to suit your needs';
-$lang['articles_no_records']         = 'There are no articles in the system.';
-$lang['articles_create_new']         = 'Create a new Articles.';
-$lang['articles_create_success']     = 'Articles successfully created.';
-$lang['articles_create_failure']     = 'There was a problem creating the articles: ';
-$lang['articles_create_new_button']  = 'Create New Articles';
-$lang['articles_invalid_id']         = 'Invalid Articles ID.';
-$lang['articles_edit_success']       = 'Articles successfully saved.';
-$lang['articles_edit_failure']       = 'There was a problem saving the articles: ';
-$lang['articles_delete_success']     = 'record(s) successfully deleted.';
-$lang['articles_delete_failure']     = 'We could not delete the record: ';
-$lang['articles_delete_error']       = 'You have not selected any records to delete.';
-$lang['articles_actions']            = 'Actions';
-$lang['articles_cancel']             = 'Cancel';
-$lang['articles_delete_record']      = 'Delete this Articles';
-$lang['articles_delete_confirm']     = 'Are you sure you want to delete this articles?';
-$lang['articles_edit_heading']       = 'Edit Articles';
+
 
 // Create/Edit Buttons
 $lang['articles_action_edit']        = 'Save Articles';
@@ -52,15 +29,3 @@ $lang['articles_column_modified_by'] = 'Modified By';
 $lang['articles_module_name']        = 'Articles';
 $lang['articles_module_description'] = 'Your module description';
 $lang['articles_area_title']         = 'Articles';
-
-// Fields
-$lang['articles_field_magazines_id'] = 'Magazines Id';
-$lang['articles_field_title']        = 'Title';
-$lang['articles_field_categories']   = 'Categories';
-$lang['articles_field_authors']      = 'Author(s)';
-$lang['articles_field_affiliation']  = 'Affiliation';
-$lang['articles_field_references']   = 'References';
-$lang['articles_field_summary']      = 'Summary';
-$lang['articles_field_tags']         = 'Tags';
-$lang['articles_field_page']         = 'Page';
-$lang['articles_field_author_email'] = 'Corresponding Author';
